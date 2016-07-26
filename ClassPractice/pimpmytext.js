0f01c79e718d4ce583b46ab95bbe28b3e26edf1a
Tingyu Yang <tingyuyang@lclark.edu>
2016-07-26 00:08:04 -0700

    Add files via upload

---
+++ ClassPractice/pimpmytext.js
@@ -0,0 +1,10 @@
+function bigger(){
+console.log("hi");
+document.getElementById("stage").style.fontSize="24pt";
+}
+
+function bling(){
+    if(document.getElementById("cb").checked){
+        alert("on");
+    }
+}
